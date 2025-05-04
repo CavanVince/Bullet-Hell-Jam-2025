eb8dd04be4161b1b4596e4b41fd7fa293592e9ca
Cavan Vince <csvince87@gmail.com>
2025-05-03 20:47:08 -0400

    Base enemy implementation 😠

---
+++ Bullet Hell/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    PlayerController player;
+    private float shotTimerTracker = 0;
+
+    [SerializeField]
+    private float shotTimer; // How long between shots
+
+    [SerializeField]
+    private float shotDistance; // How far the enemy can be before it shoots
+
+    void Start()
+    {
+        // TODO: Needs to be made to not suck
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (shotTimerTracker <= 0)
+        {
+            ShootPlayer();
+            shotTimerTracker = shotTimer;
+        }
+        else
+        {
+            shotTimerTracker -= Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Launches a bullet at the player if 
+    /// </summary>
+    protected virtual void ShootPlayer()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) < shotDistance)
+        {
+            BulletManager.instance.FireBullet(transform.position, (player.transform.position - transform.position).normalized);
+        }
+    }
+}
